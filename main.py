207a5766f3e1067264e2e267569f2f9a8bbc3632
Lisanudeensait <lisansait04@gmail.com>
2026-08-02 18:49:25 +0530

    Day - 2 Expense Tracker User Input

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Welcome to personal Expense Tracker")
+print("==== Personal Expense Tracker =====")
+
+name = input("Enter your name:")
+expense = int(input("Enter your expense amount:"))
+print("Welcome",name)
+
+print("Expense Amount:",expense)
